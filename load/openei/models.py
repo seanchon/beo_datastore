a480cf2cca3ea81554ea72df51877a13a6dfec54
Sean Chon <sean.chon@tvrpllc.com>
2019-04-25 15:51:47 -0700

    [BEO-41] Create process to create 288 dataframes for load data filtered by start, end, weekend, and weekday.

--- load/openei/models.py
+++ load/openei/models.py
@@ -143,28 +143,28 @@ class ReferenceBuilding(ValidationModel):
         """
         Returns a 12 x 24 dataframe of totals (sums).
         """
-        return self.intervalframe.total_288_dataframe
+        return self.intervalframe.total_frame288.dataframe
 
     @property
     def average_288(self):
         """
         Returns a 12 x 24 dataframe of averages.
         """
-        return self.intervalframe.average_288_dataframe
+        return self.intervalframe.average_frame288.dataframe
 
     @property
     def peak_288(self):
         """
         Returns a 12 x 24 dataframe of peaks.
         """
-        return self.intervalframe.maximum_288_dataframe
+        return self.intervalframe.maximum_frame288.dataframe
 
     @property
     def count_288(self):
         """
         Returns a 12 x 24 dataframe of counts.
         """
-        return self.intervalframe.count_288_dataframe
+        return self.intervalframe.count_frame288.dataframe
 
 
 class ReferenceBuildingIntervalFrame(IntervalFrameFile):
