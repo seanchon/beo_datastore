aaeb404a55e40ed83580ea96765551d3b4128277
Sean Chon <sean.chon@tvrpllc.com>
2019-06-03 11:27:58 -0700

    [BEO-62] Build algorithm for peak-shaving optimizations on a daily/monthly basis.

--- beo_datastore/libs/battery.py
+++ beo_datastore/libs/battery.py
@@ -1,11 +1,17 @@
 from cached_property import cached_property
 from collections import OrderedDict
 from datetime import timedelta
-from functools import lru_cache as cache
-from math import floor
+from itertools import repeat
+from functools import lru_cache
+from math import ceil, floor
+from multiprocessing import Pool
+import numpy as np
 import pandas as pd
 
-from beo_datastore.libs.intervalframe import ValidationIntervalFrame
+from beo_datastore.libs.intervalframe import (
+    ValidationFrame288,
+    ValidationIntervalFrame,
+)
 from beo_datastore.libs.utils import timedelta_to_hours
 
 
@@ -225,6 +231,19 @@ class BatteryIntervalFrame(ValidationIntervalFrame):
         return self.load_intervalframe + self
 
     @property
+    def combined_dataframe(self):
+        """
+        Return load plus battery operations in a dataframe.
+        """
+        return pd.merge(
+            self.dataframe,
+            self.load_intervalframe.dataframe,
+            how="inner",
+            left_index=True,
+            right_index=True,
+        ).rename(columns={"kw_x": "kw", "kw_y": "load kw"})
+
+    @property
     def load_start_timestamp(self):
         return self.load_intervalframe.start_timestamp
 
@@ -300,9 +319,9 @@ class FixedScheduleBatteryIntervalFrame(BatteryIntervalFrame):
     Battery schedule generator for fixed charging and discharging based on 288
     schedules (set operations per hours of the day each month).
 
-    - self.min_threshold_frame288 dictates kw values such that anytime a meter
+    - self.charge_schedule dictates kw values such that anytime a meter
     is reporting a lower power level, the battery attempts to charge.
-    - self.max_threshold_frame288 dictates kw values such that anytime a meter
+    - self.discharge_schedule dictates kw values such that anytime a meter
     is reporting a higher power level, the battery attempts to discharge.
     """
 
@@ -310,8 +329,8 @@ class FixedScheduleBatteryIntervalFrame(BatteryIntervalFrame):
         self,
         battery,
         load_intervalframe,
-        min_threshold_frame288,
-        max_threshold_frame288,
+        charge_schedule,
+        discharge_schedule,
         *args,
         **kwargs
     ):
@@ -321,14 +340,14 @@ class FixedScheduleBatteryIntervalFrame(BatteryIntervalFrame):
 
         :param battery: Battery
         :param load_intervalframe: ValidationIntervalFrame
-        :param min_threshold_frame288: ValidationFrame288
-        :param max_threshold_frame288: ValidationFrame288
+        :param charge_schedule: ValidationFrame288
+        :param discharge_schedule: ValidationFrame288
         """
         self.cached_operations = []
         self.cached_operation_timestamps = []
         self.cached_battery_charge = battery.charge
-        self.min_threshold_frame288 = min_threshold_frame288
-        self.max_threshold_frame288 = max_threshold_frame288
+        self.charge_schedule = charge_schedule
+        self.discharge_schedule = discharge_schedule
 
         super().__init__(
             battery,
@@ -352,7 +371,7 @@ class FixedScheduleBatteryIntervalFrame(BatteryIntervalFrame):
             return self.load_start_timestamp
 
     @staticmethod
-    @cache(maxsize=None)
+    @lru_cache(maxsize=None)
     def _generate_battery_operations(
         power,
         duration,
@@ -470,9 +489,9 @@ class FixedScheduleBatteryIntervalFrame(BatteryIntervalFrame):
         """
         Generate full charge/discharge sequence.
 
-            - When power is below self.min_threshold_frame288, battery will
+            - When power is below self.charge_schedule, battery will
             attempt to charge.
-            - When power is above self.max_threshold_frame288, battery will
+            - When power is above self.discharge_schedule, battery will
             attempt to discharge.
         """
         for index, row in self.load_intervalframe.filter_by_datetime(
@@ -483,21 +502,294 @@ class FixedScheduleBatteryIntervalFrame(BatteryIntervalFrame):
                 time_gap = index - self.next_interval_timestamp
                 self.operate_battery(0, time_gap)
 
-            min_threshold = self.min_threshold_frame288.dataframe[index.month][
+            charge_threshold = self.charge_schedule.dataframe[index.month][
                 index.hour
             ]
-            max_threshold = self.max_threshold_frame288.dataframe[index.month][
-                index.hour
-            ]
-            if row.kw < min_threshold:  # charge battery
+            discharge_threshold = self.discharge_schedule.dataframe[
+                index.month
+            ][index.hour]
+            if row.kw < charge_threshold:  # charge battery
                 self.operate_battery(
-                    floor(min_threshold - row.kw), self.load_period
+                    floor(charge_threshold - row.kw), self.load_period
                 )
-            elif row.kw > max_threshold:  # discharge battery
+            elif row.kw > discharge_threshold:  # discharge battery
                 self.operate_battery(
-                    floor(max_threshold - row.kw), self.load_period
+                    floor(discharge_threshold - row.kw), self.load_period
                 )
             else:  # no operation
                 self.operate_battery(0, self.load_period)
 
         self._commit_battery_operations()
+
+    def compare_peak_loads(self):
+        """
+        Return dataframe consisting of peak loads by month before and after
+        application of battery charge and discharge schedules.
+
+        :return: pandas DataFrame
+        """
+        before = pd.DataFrame(
+            self.load_intervalframe.maximum_frame288.dataframe.max()
+        )
+        after = pd.DataFrame(
+            self.combined_intervalframe.maximum_frame288.dataframe.max()
+        )
+
+        df = pd.merge(
+            before, after, how="inner", left_index=True, right_index=True
+        ).rename(columns={"0_x": "before", "0_y": "after"})
+        df["net"] = df["after"] - df["before"]
+
+        return df
+
+    def compare_month_hours(self, month, aggfunc):
+        """
+        Return dataframe consisting of hourly values based on aggfunc
+        before and after application of battery charge and discharge schedules.
+
+        :param month: integer
+        :param aggfunc: aggregation function
+        :return: pandas DataFrame
+        """
+        before = self.load_intervalframe.compute_frame288(aggfunc).dataframe[
+            month
+        ]
+        after = self.combined_intervalframe.compute_frame288(
+            aggfunc
+        ).dataframe[month]
+
+        before_column = "{}_x".format(month)
+        after_column = "{}_y".format(month)
+        df = pd.merge(
+            before, after, how="inner", left_index=True, right_index=True
+        ).rename(columns={before_column: "before", after_column: "after"})
+        df["net"] = df["after"] - df["before"]
+
+        return df
+
+
+class PeakShavingSimulator(object):
+    """
+    Simulator to run various optimization scenarios for when to charge and
+    discharge and at what power thresholds.
+    """
+
+    @classmethod
+    def get_peak_power(
+        cls,
+        battery,
+        load_intervalframe,
+        month,
+        charge_threshold,
+        discharge_threshold,
+    ):
+        """
+        Simulate battery operations on a single month and return resulting peak
+        load at input discharge threshold. This is useful for finding the
+        optimal discharge threshold resulting in the smallest resulting peak
+        load.
+
+        :param battery: Battery
+        :param load_intervalframe: ValidationIntervalFrame
+        :param month: integer
+        :param charge_threshold: integer
+        :param discharge_threshold: integer
+        :return: (discharge threshold, resulting peak load)
+        """
+        load_intervalframe = load_intervalframe.filter_by_months({month})
+
+        charge_schedule = ValidationFrame288.convert_matrix_to_frame288(
+            [[charge_threshold] * 24] * 12
+        )
+        discharge_schedule = ValidationFrame288.convert_matrix_to_frame288(
+            [[discharge_threshold] * 24] * 12
+        )
+
+        battery_intervalframe = FixedScheduleBatteryIntervalFrame(
+            battery=battery,
+            load_intervalframe=load_intervalframe,
+            charge_schedule=charge_schedule,
+            discharge_schedule=discharge_schedule,
+        )
+        battery_intervalframe.generate_full_sequence()
+
+        combined_maximum_frame288 = (
+            battery_intervalframe.combined_intervalframe.maximum_frame288
+        )
+        return (
+            discharge_threshold,
+            combined_maximum_frame288.dataframe[month].max(),  # peak load
+        )
+
+    @classmethod
+    def optimize_discharge_threshold(
+        cls,
+        battery,
+        load_intervalframe,
+        month,
+        charge_threshold,
+        number_of_checks=None,
+    ):
+        """
+        Based on a given month and given charge threshold, find best discharge
+        threshold for peak shaving.
+
+        :param battery: Battery
+        :param load_intervalframe: ValidationIntervalFrame
+        :param month: integer
+        :param charge_threshold: fixed level to charge below (int)
+        :param number_of_checks: number of discharge thresholds to try (int)
+        :return: (discharge threshold, peak load)
+        """
+        intervalframe = load_intervalframe.filter_by_months({month})
+        max_load = intervalframe.maximum_frame288.dataframe[month].max()
+
+        # OPTIMIZE: Can fewer thresholds be checked?
+        if number_of_checks is None:
+            # tries discharge thresholds at 1kw increments
+            number_of_checks = int(battery.rating)
+        discharge_thresholds = set(
+            [int(max_load - x) for x in range(1, number_of_checks + 1)]
+        )
+
+        # get resulting peak powers (multiprocess)
+        with Pool() as pool:
+            results = pool.starmap(
+                cls.get_peak_power,
+                zip(
+                    repeat(battery),
+                    repeat(intervalframe),
+                    repeat(month),
+                    repeat(charge_threshold),
+                    discharge_thresholds,
+                ),
+            )
+
+        # return highest threshold with lowest resulting peak
+        ranked_results = {}
+        for discharge_threshold, peak_power in results:
+            if peak_power not in ranked_results.keys():
+                ranked_results[peak_power] = set()
+            ranked_results[peak_power].add(discharge_threshold)
+        return (
+            max(ranked_results[min(ranked_results.keys())]),  # threshold
+            min(ranked_results.keys()),  # resulting peak power
+        )
+
+    @classmethod
+    def optimize_schedules_with_exports(cls, battery, load_intervalframe):
+        """
+        Creates optimal monthly charge and discharge schedules to shave peak
+        loads based on charging using meter energy exports only.
+
+        :param battery: Battery
+        :param load_intervalframe: ValidationIntervalFrame
+        :return: (charge schedule, discharge schedule)
+            (ValidationFrame288, ValidationFrame288)
+        """
+        # run optimization on smaller dataset for speed
+        load_intervalframe = load_intervalframe.resample_intervalframe(
+            "60min", np.mean
+        )
+
+        results = {"charge_threshold": {}, "discharge_threshold": {}}
+        for month in set(load_intervalframe.dataframe.index.month):
+            results["charge_threshold"][month] = 0
+            results["discharge_threshold"][
+                month
+            ], _ = cls.optimize_discharge_threshold(
+                battery, load_intervalframe, month, 0
+            )
+
+        return (
+            ValidationFrame288.convert_matrix_to_frame288(
+                [
+                    [results["charge_threshold"].get(month, 0)] * 24
+                    for month in range(1, 13)
+                ]
+            ),
+            ValidationFrame288.convert_matrix_to_frame288(
+                [
+                    [results["discharge_threshold"].get(month, 0)] * 24
+                    for month in range(1, 13)
+                ]
+            ),
+        )
+
+    @classmethod
+    def optimize_schedules_with_grid(
+        cls, battery, load_intervalframe, verbose=False
+    ):
+        """
+        Creates optimal monthly charge and discharge schedules to shave peak
+        loads based on charging using energy exports and grid energy.
+
+        :param battery: Battery
+        :param load_intervalframe: ValidationIntervalFrame
+        :param verbose: if True, print optimization steps
+        :return: (charge schedule, discharge schedule)
+            (ValidationFrame288, ValidationFrame288)
+        """
+        # run optimization on smaller dataset for speed
+        load_intervalframe = load_intervalframe.resample_intervalframe(
+            "60min", np.mean
+        )
+
+        results = {"charge_threshold": {}, "discharge_threshold": {}}
+        for month in set(load_intervalframe.dataframe.index.month):
+            if verbose:
+                print("Month: {}".format(month))
+            month_intervalframe = load_intervalframe.filter_by_months({month})
+            peak_load = np.max(month_intervalframe.dataframe).kw
+
+            # OPTIMIZE: Is there a bettery starting list of charge thresholds?
+            # create list of charge thresholds to simulate
+            increment = ceil(min(peak_load, battery.rating) / 5)
+            if peak_load < battery.rating:
+                charge_thresholds = range(1, int(peak_load), increment)
+            else:
+                charge_thresholds = range(
+                    max(1, int(peak_load - battery.rating)),
+                    int(peak_load),
+                    increment,
+                )
+
+            lowest_peak = float("inf")
+            for charge_threshold in charge_thresholds:
+                discharge_threshold, peak = cls.optimize_discharge_threshold(
+                    battery, load_intervalframe, month, charge_threshold
+                )
+                if verbose:
+                    print(
+                        "Charge Threshold: {}, Discharge Threshold: {}, "
+                        "Net Load: {}, Peak Load: {}".format(
+                            charge_threshold,
+                            discharge_threshold,
+                            peak,
+                            peak_load,
+                        )
+                    )
+
+                if peak < lowest_peak:
+                    results["charge_threshold"][month] = charge_threshold
+                    results["discharge_threshold"][month] = discharge_threshold
+                    lowest_peak = peak
+                elif peak == peak_load:
+                    continue
+                else:
+                    break
+
+        return (
+            ValidationFrame288.convert_matrix_to_frame288(
+                [
+                    [results["charge_threshold"].get(month, 0)] * 24
+                    for month in range(1, 13)
+                ]
+            ),
+            ValidationFrame288.convert_matrix_to_frame288(
+                [
+                    [results["discharge_threshold"].get(month, 0)] * 24
+                    for month in range(1, 13)
+                ]
+            ),
+        )
