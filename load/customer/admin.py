9da47d527d98c64b56fc66e0999744d690070c60
Sean Chon <sean@terraverde.energy>
2019-08-09 11:26:19 -0700

    [BEO-107] Create CustomerPopulation and CustomerCluster models.

--- load/customer/admin.py
+++ load/customer/admin.py
@@ -4,7 +4,12 @@ from __future__ import unicode_literals
 from django.contrib import admin
 
 from der.simulation.models import StoredBatterySimulation
-from load.customer.models import Meter, Channel
+from load.customer.models import (
+    Meter,
+    Channel,
+    CustomerPopulation,
+    CustomerCluster,
+)
 
 
 class ChannelInline(admin.TabularInline):
@@ -49,4 +54,32 @@ class ChannelAdmin(admin.ModelAdmin):
         "meter__sa_id",
         "meter__rate_plan_name",
         "meter__load_serving_entity__name",
+    ]
+
+
+class CustomerClusterInline(admin.TabularInline):
+    model = CustomerCluster
+    readonly_fields = [
+        "id",
+        "cluster_id",
+        "number_of_meters",
+        "frame288_html_plot",
+    ]
+    exclude = ["meters"]
+
+
+@admin.register(CustomerPopulation)
+class CustomerPopulationAdmin(admin.ModelAdmin):
+    readonly_fields = ["number_of_clusters"]
+    inlines = [CustomerClusterInline]
+    search_fields = ["name", "frame288_type", "load_serving_entity__name"]
+
+
+@admin.register(CustomerCluster)
+class CustomerClusterAdmin(admin.ModelAdmin):
+    readonly_fields = ["number_of_meters", "frame288_html_plot"]
+    search_fields = [
+        "customer_population__name",
+        "customer_population__frame288_type",
+        "customer_population__load_serving_entity__name",
     ]
