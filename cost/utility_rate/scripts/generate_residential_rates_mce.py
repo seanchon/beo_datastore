9a61997a8e0ec64fef43f5094fe0ec53ef97de22
Sean Chon <sean@terraverde.energy>
2020-04-09 09:50:14 -0700

    Ingest MCE residential rates effective 2019/07/01.

--- cost/utility_rate/scripts/generate_residential_rates_mce.py
+++ cost/utility_rate/scripts/generate_residential_rates_mce.py
@@ -3,9 +3,10 @@ import json
 
 
 SOURCE = (
-    "https://www.mcecleanenergy.org/wp-content/uploads/2017/03/"
-    "MCE_Residential_Rates_Apr2017.pdf"
+    "https://www.mcecleanenergy.org/wp-content/uploads/2019/12/"
+    "MCE-residential-rates_070119.pdf"
 )
+EFFECTIVE_DATETIME = datetime(2019, 7, 1, 0, 0)
 
 BASIC_SCHED = [[0] * 24] * 12
 
@@ -127,6 +128,25 @@ EV_WEEKEND_SCHED = [
     [5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 3, 3, 3, 3, 5, 5, 5, 5, 5],
 ]
 
+# EV keys:
+# 0 - Summer Peak, 1 - Summer Part-Peak, 2 - Summer Off-Peak,
+# 3 - Winter Peak, 4, Winter Part-Peak, 5 Winter Off-Peak
+EV_2_WEEKDAY_SCHED = [
+    [5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 4, 3, 3, 3, 3, 3, 4, 4, 4],
+    [5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 4, 3, 3, 3, 3, 3, 4, 4, 4],
+    [5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 4, 3, 3, 3, 3, 3, 4, 4, 4],
+    [5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 4, 3, 3, 3, 3, 3, 4, 4, 4],
+    [5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 4, 3, 3, 3, 3, 3, 4, 4, 4],
+    [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 0, 0, 0, 0, 0, 1, 1, 1],
+    [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 0, 0, 0, 0, 0, 1, 1, 1],
+    [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 0, 0, 0, 0, 0, 1, 1, 1],
+    [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 0, 0, 0, 0, 0, 1, 1, 1],
+    [5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 4, 3, 3, 3, 3, 3, 4, 4, 4],
+    [5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 4, 3, 3, 3, 3, 3, 4, 4, 4],
+    [5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 4, 3, 3, 3, 3, 3, 4, 4, 4],
+]
+EV_2_WEEKEND_SCHED = EV_2_WEEKDAY_SCHED
+
 
 def create_energy_dict(
     energy_key_vals=None,
@@ -148,7 +168,7 @@ def create_energy_dict(
 def run(*args):
     """
     Usage:
-        - python manage.py runscript cost.utility_rate.scripts.generate_mce_residential_rates --script-args DESTINATION
+        - python manage.py runscript cost.utility_rate.scripts.generate_residential_rates_mce --script-args DESTINATION
     """
     if len(args) < 1:
         print(
@@ -166,17 +186,17 @@ def run(*args):
     for name, rate_list, weekday_sched, weekend_sched in [
         (
             "E1, EM, ES, ESR, ET, Basic Residential",
-            [{"key": "All Electric Usage", "val": 0.068}],
+            [{"key": "All Electric Usage", "val": 0.087}],
             BASIC_SCHED,
             BASIC_SCHED,
         ),
         (
             "E-TOUA, Residential Time-of-Use",
             [
-                {"key": "Summer Peak", "val": 0.153},
-                {"key": "Summer Off-Peak", "val": 0.078},
-                {"key": "Winter Peak", "val": 0.066},
-                {"key": "Winter Off-Peak", "val": 0.052},
+                {"key": "Summer Peak", "val": 0.170},
+                {"key": "Summer Off-Peak", "val": 0.094},
+                {"key": "Winter Peak", "val": 0.083},
+                {"key": "Winter Off-Peak", "val": 0.069},
             ],
             TOU_A_WEEKDAY_SCHED,
             TOU_A_WEEKEND_SCHED,
@@ -184,22 +204,33 @@ def run(*args):
         (
             "E-TOUB, Residential Time-of-Use",
             [
-                {"key": "Summer Peak", "val": 0.178},
-                {"key": "Summer Off-Peak", "val": 0.072},
-                {"key": "Winter Peak", "val": 0.069},
-                {"key": "Winter Off-Peak", "val": 0.049},
+                {"key": "Summer Peak", "val": 0.191},
+                {"key": "Summer Off-Peak", "val": 0.089},
+                {"key": "Winter Peak", "val": 0.085},
+                {"key": "Winter Off-Peak", "val": 0.066},
             ],
             TOU_B_WEEKDAY_SCHED,
             TOU_B_WEEKEND_SCHED,
         ),
         (
+            "E-TOUC3, Residential Time-of-Use",
+            [
+                {"key": "Summer Peak", "val": 0.145},
+                {"key": "Summer Off-Peak", "val": 0.082},
+                {"key": "Winter Peak", "val": 0.089},
+                {"key": "Winter Off-Peak", "val": 0.071},
+            ],
+            TOU_C_WEEKDAY_SCHED,
+            TOU_C_WEEKEND_SCHED,
+        ),
+        (
             "E6, EM-TOU, Residential Time-of-Use",
             [
-                {"key": "Summer Peak", "val": 0.186},
-                {"key": "Summer Part-Peak", "val": 0.082},
-                {"key": "Summer Off-Peak", "val": 0.043},
-                {"key": "Winter Part-Peak", "val": 0.065},
-                {"key": "Winter Off-Peak", "val": 0.052},
+                {"key": "Summer Peak", "val": 0.223},
+                {"key": "Summer Part-Peak", "val": 0.105},
+                {"key": "Summer Off-Peak", "val": 0.057},
+                {"key": "Winter Part-Peak", "val": 0.084},
+                {"key": "Winter Off-Peak", "val": 0.071},
             ],
             EM_WEEKDAY_SCHED,
             EM_WEEKEND_SCHED,
@@ -207,15 +238,28 @@ def run(*args):
         (
             "EV, Residential Rates for Electric Vehicle Owners",
             [
-                {"key": "Summer Peak", "val": 0.200},
-                {"key": "Summer Part-Peak", "val": 0.075},
-                {"key": "Summer Off-Peak", "val": 0.030},
-                {"key": "Winter Peak", "val": 0.055},
-                {"key": "Winter Part-Peak", "val": 0.030},
-                {"key": "Winter Off-Peak", "val": 0.030},
+                {"key": "Summer Peak", "val": 0.247},
+                {"key": "Summer Part-Peak", "val": 0.103},
+                {"key": "Summer Off-Peak", "val": 0.037},
+                {"key": "Winter Peak", "val": 0.073},
+                {"key": "Winter Part-Peak", "val": 0.034},
+                {"key": "Winter Off-Peak", "val": 0.039},
             ],
             EV_WEEKDAY_SCHED,
             EV_WEEKEND_SCHED,
+        ),
+        (
+            "EV 2, Residential Rates for Electric Vehicle Owners",
+            [
+                {"key": "Summer Peak", "val": 0.154},
+                {"key": "Summer Part-Peak", "val": 0.110},
+                {"key": "Summer Off-Peak", "val": 0.069},
+                {"key": "Winter Peak", "val": 0.098},
+                {"key": "Winter Part-Peak", "val": 0.085},
+                {"key": "Winter Off-Peak", "val": 0.062},
+            ],
+            EV_2_WEEKDAY_SCHED,
+            EV_2_WEEKEND_SCHED,
         ),
     ]:
         energy_key_vals = []
@@ -250,7 +294,7 @@ def run(*args):
             rate_data[i]["sourceReference"] = SOURCE
         rate_data[i]["sector"] = "Residential"
         rate_data[i]["effectiveDate"] = {
-            "$date": int(datetime(2017, 4, 1, 0, 0).timestamp() * 1000)
+            "$date": int(EFFECTIVE_DATETIME.timestamp() * 1000)
         }
 
     with open(destination, "w") as fp:
