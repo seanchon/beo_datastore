a3f9ea839d558caf6c1d2a5b29359f5f0e560845
Milo Toor <milo.toor@gmail.com>
2020-05-01 17:01:22 -0700

    Lint

--- ws/consumers.py
+++ ws/consumers.py
@@ -26,7 +26,6 @@ class ScenarioUpdatesConsumer(AsyncJsonWebsocketConsumer):
         Joins the `scenario_update` channel group
         """
         await self.channel_layer.group_add(self.group_name, self.channel_name)
-
         await self.accept()
 
     async def disconnect(self, close_code):
