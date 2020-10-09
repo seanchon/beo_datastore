a8f4983ca22ddbbfb92b137ce359c908b0033746
Sean Chon <sean@terraverde.energy>
2020-10-09 14:09:19 -0700

    [BEO-474] Add celery beat job to detect and re-run jobs that have not completed.

--- cost/tasks.py
+++ cost/tasks.py
@@ -3,7 +3,11 @@ from pytz import timezone
 
 from celery.utils.log import get_task_logger
 
+from django.core.mail import send_mail
+from django.utils.timezone import now
+
 from beo_datastore.celery import app
+from beo_datastore.settings import ADMINS, APP_URL
 
 from cost.procurement.models import CAISOReport
 from cost.study.models import Scenario
@@ -19,6 +23,11 @@ def run_scenario(scenario_id):
     Perform a run() operation on a Scenario.
     """
     scenario = Scenario.objects.get(id=scenario_id)
+
+    # set locked_unlocked_at for rerun_incomplete_scenarios() check
+    scenario.locked_unlocked_at = now()
+    scenario.save()
+
     for meter in scenario.meters.all():
         run_simulation_and_cost.delay(
             scenario_id=scenario.id, meter_id=meter.id
@@ -35,31 +44,71 @@ def run_simulation_and_cost(scenario_id, meter_id):
         meter=Meter.objects.get(id=meter_id)
     )
 
-    if scenario.simulations_completed and scenario.cost_calculations_completed:
+    if (
+        not Scenario.is_locked(scenario_id)
+        and scenario.simulations_completed
+        and scenario.cost_calculations_completed
+    ):
         generate_intervalframe_and_reports.delay(scenario.id)
 
 
 @app.task(soft_time_limit=1800, max_retries=3)
-def generate_intervalframe_and_reports(study_id, overwrite=False):
+def generate_intervalframe_and_reports(scenario_id, overwrite=False):
     """
     Store post_der_intervalframe as meter_intervalframe
 
     :param scenario_id: Scenario id
     :param force: force recomputation
     """
-    locked = Scenario.is_locked(study_id)
-    completed = Scenario.is_completed(study_id)
+    locked = Scenario.is_locked(scenario_id)
+    completed = Scenario.is_completed(scenario_id)
 
     if locked or (completed and not overwrite):
         # do nothing if job is running or completed
         return
 
-    scenario = Scenario.objects.get(id=study_id)
+    scenario = Scenario.objects.get(id=scenario_id)
     scenario.aggregate_meter_intervalframe()
     scenario.generate_reports()
 
     if scenario.has_completed:
         scenario.mark_complete()
+
+
+@app.task()
+def rerun_incomplete_scenarios(older_than_minutes: int):
+    """
+    Scans for and re-runs incomplete scenarios where:
+        - scenario.completed is False
+        - scenario.locked_unlocked_at timestamp is older_than_minutes, which
+          specifies that the scenario has not completed in older_than_minutes.
+
+    This is meant as a celery periodic task:
+        - see: APP_URL/admin/django_celery_beat/periodictask/
+
+    Email is sent to application ADMINS noting which scenario have been re-run.
+    """
+    incomplete_scenarios = Scenario.objects.filter(
+        completed=False,
+        locked_unlocked_at__lte=(
+            now() - timedelta(minutes=older_than_minutes)
+        ),
+    )
+    incomplete_ids = incomplete_scenarios.values_list("id", flat=True)
+
+    for scenario in incomplete_scenarios:
+        run_scenario.delay(scenario.id)
+
+    if incomplete_ids:
+        send_mail(
+            subject="Scenarios re-run on {}".format(APP_URL),
+            message="The following scenarios were re-run:\n{}".format(
+                "\n".join([str(x) for x in incomplete_ids])
+            ),
+            from_email=None,
+            recipient_list=[x[1] for x in ADMINS],
+            fail_silently=False,
+        )
 
 
 @app.task(soft_time_limit=1800)
