71dafae49448934055807d66bfa16d135d0689fd
bnikanjam <babak.nikanjam@gmail.com>
2020-12-22 15:23:02 -0800

    Refactor FuelSwitching configuration and strategy ingestion

--- der/simulation/scripts/seed_fuel_switching.py
+++ der/simulation/scripts/seed_fuel_switching.py
@@ -1,3 +1,5 @@
+import csv
+import re
 from pathlib import Path
 
 import pandas as pd
@@ -18,16 +20,20 @@ def create_strategies():
     print("\nFuel-Switching Strategies:")
     for openei_file in openei_csv_files:
         building_profile_name = openei_file.name
-        dataframe = pd.read_csv(openei_file)
+        dataframe = pd.read_csv(openei_file, skiprows=1)
         errors, _ = TMY3Parser.validate(dataframe)
         if errors:
             print(f"Failed : {building_profile_name} -> {errors}")
+            continue
 
         name = building_profile_name.strip().replace("_", " ")
         name = name[0].title() + name[1:]
         strategy, created = FuelSwitchingStrategy.get_or_create(
             name=name,
-            description="Preloaded by admin",
+            description=get_strategy_description(
+                building_profile_name=building_profile_name,
+                openei_file=openei_file,
+            ),
             load_serving_entity=None,
             dataframe=dataframe,
         )
@@ -35,7 +41,6 @@ def create_strategies():
 
 
 def create_configurations():
-
     configurations = {
         "Heat Pump": {
             "space_heating": True,
@@ -67,9 +72,37 @@ def create_configurations():
 
 def prompt(obj, created):
     if created:
-        print(f"Created: {obj.name}")
+        print(f"Created new strategy:    {obj.name}")
     else:
-        print(f"Existed: {obj.name}")
+        print(f"Found existing strategy: {obj.name}")
+
+
+def get_strategy_description(building_profile_name: str, openei_file: Path):
+    # Read file URL from first cell of the first row
+    reader = csv.reader(open(openei_file))
+    file_url = next(reader)[0]
+
+    if "residential" in building_profile_name:
+        match = re.match(r"(.*) \(residential", building_profile_name)
+        if not match:
+            return None
+
+        city_name = match.group(1)
+        return (
+            "Reference load data for an average residential customer in "
+            f"{city_name}. Source file can be found here: {file_url}"
+        )
+    else:
+        match = re.match(r"(.*) in (.*) \(New 2004", building_profile_name)
+        if not match:
+            return None
+
+        building_type = match.group(1)
+        city_name = match.group(2)
+        return (
+            f"Reference load data for an average {building_type} in "
+            f"{city_name}. Source file can be found here: {file_url}"
+        )
 
 
 def run():
